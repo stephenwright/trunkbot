2809f24d10b4453889cb37d5a5dad548f493bb46
Gordon Summer <gordon.summer@gmail.com>
2011-12-22 11:21:39 -0500

    added history message assembler for everyone!

---
+++ cmd/histomatic.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+nick = ARGV[0] 
+
+if nick
+
+  require File.join( File.dirname( __FILE__ ), '../conf.rb' )
+
+  log = $conf[:dir][:root] + "dict/#{nick}.log"
+  s = ''
+
+  if File.exists? log
+
+    phrases = []
+    f = File.new( log, "r" )
+    f.each{|line| phrases << line.chomp }
+
+    s += '  ' + phrases[rand(phrases.length)] + "\n"
+    s += '  ' + phrases[rand(phrases.length)] + "\n"
+    s += '  ' + phrases[rand(phrases.length)] + "\n     - #{nick}"
+
+  else
+    s = 'no log for that user'
+  end
+
+  puts s
+
+else
+  puts "usage: histomatic <nick>"
+end
+
