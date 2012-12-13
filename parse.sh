59a33ab720348bc858977741b2e9203297f780c8
Stephen Wright <stephen.c.wright@gmail.com>
2012-12-12 22:01:06 -0500

    added turdburg to log parsing for histo

--- parse.sh
+++ parse.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NICKS=('derjur' 'dr_summer' 'beeeee' 'chelsea_' 'ol_qwerty_bastrd' 'vladTO')
+NICKS=('derjur' 'dr_summer' 'beeeee' 'chelsea_' 'ol_qwerty_bastrd' 'vladTO' 'Turdburg')
 
 for n in "${NICKS[@]}"; do
 	grep "b33r_time.${n}:" log/b33r* | sed "s/^log\/b33r_time.*${n}://" | sort -u > dict/q.${n}.log
