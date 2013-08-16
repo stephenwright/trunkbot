8908e68652559f07690742a062940ae4ca9edb9b
Stephen Wright <stephen.c.wright@gmail.com>
2013-08-16 15:07:45 -0400

    added tuxotaku to parse command

--- parse.sh
+++ parse.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-NICKS=('derjur' 'dr_summer' 'beeeee' 'chelsea_' 'ol_qwerty_bastrd' 'vladTO' 'Turdburg')
+NICKS=('TuxOtaku' 'derjur' 'dr_summer' 'beeeee' 'chelsea_' 'ol_qwerty_bastrd' 'vladTO' 'Turdburg')
 
 for n in "${NICKS[@]}"; do
-	grep "b33r_time.${n}:" log/b33r* | sed "s/^log\/b33r_time.*${n}://" | sort -u > dict/q.${n}.log
+	grep "b33r_time.${n}:" log/**/b33r* | sed "s/^log.*\/b33r_time.*${n}://" | sort -u > dict/q.${n}.log
+	grep "b33r_time.${n}:" log/b33r* | sed "s/^log.*\/b33r_time.*${n}://" | sort -u > dict/q.${n}.log
 done
 
